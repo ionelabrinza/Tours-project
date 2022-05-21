5156db15ae4d9d86d67912f21046a116ab0e069f
Nicoleta Brinza <ionela.brinza@yahoo.com>
2022-05-20 23:06:48 -0500

    remove tour and added refresh button

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Tour = ({id,image,info,price,name}) => {
+const Tour = ({id,image,info,price,name, removeTour}) => {
   const [readMore, setReadMore] = useState(false)
   return <article className="single-tour">
   <img src={image} alt={name}/>
@@ -13,7 +13,7 @@ const Tour = ({id,image,info,price,name}) => {
     `}
     <button onClick={() => setReadMore(!readMore)}>{readMore?'show less':'read more'}</button>
     </p>
-    <button className="delete-btn">not interested</button>
+    <button className="delete-btn" onClick={()=> removeTour(id)}>not interested</button>
   </footer>
   </article>;
 };
